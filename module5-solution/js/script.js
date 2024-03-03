6b91360b4767744183a62ccc775e2ce280cc55ed
David <david@Davids-MacBook-Air.local>
2024-03-03 13:51:47 +0100

    S6

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -119,7 +119,7 @@ function buildAndShowHomeHTML (categories) {
       // var homeHtmlToInsertIntoMainPage = ....
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,
         "short_name",
-         "{{"+ chosenCategoryShortName +"}}")
+         "'"+ chosenCategoryShortName +"'")
 
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
